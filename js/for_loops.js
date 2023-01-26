9b3175ea83d635f4a272be9e897986907e941994
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-01-26 16:09:03 -0600

    number 5

--- js/for_loops.js
+++ js/for_loops.js
@@ -18,13 +18,17 @@ console.log("Just making sure it works");
 //         console.log(randomNumber + " is odd!");
 //     }
 // }
-
-for (let i = 1; i < 10; i++) {
-    let numbers = "";
-    for (let x = 0; x < i; x++) {
-        numbers += i;
-    }
-    console.log(numbers);
-}
+//
+// for (let i = 1; i < 10; i++) {
+//     let numbers = "";
+//     for (let x = 0; x < i; x++) {
+//         numbers += i;
+//     }
+//     console.log(numbers);
+// }
+//
+// for (let i = 100; i >= 5; i -= 5) {
+//     console.log(i);
+// }
 
 
