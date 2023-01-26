032945ed702d5a5838738c1401bf85611369bdd5
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-01-26 15:44:47 -0600

    1 22 333 4444

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,20 +8,23 @@ console.log("Just making sure it works");
 // }
 //
 // showMultiplicationTable(7);
-for (var i = 20; i < 200; i++);
-    var randomNumber = 20;
-    for (var x = 20; x < Math.floor((Math.random() * 200) + 20); x++) {
-        randomNumber++;
-        if (randomNumber % 2 === 0) {
-            console.log(randomNumber + " is even!");
-        }   else if (randomNumber % 2 !== 0) {
-            console.log(randomNumber + " is odd!");
-        }
 
 
+// for (let i = 0; i < 10; i++) {
+//     let randomNumber = Math.floor(Math.random() * 181) + 20;
+//     if (randomNumber % 2 === 0) {
+//         console.log( randomNumber + " is even!");
+//     } else {
+//         console.log(randomNumber + " is odd!");
+//     }
+// }
+
+for (let i = 1; i < 10; i++) {
+    let numbers = "";
+    for (let x = 0; x < i; x++) {
+        numbers += i;
     }
-
-
-    //
+    console.log(numbers);
+}
 
 
