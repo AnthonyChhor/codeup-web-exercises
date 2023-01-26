514b157f9902e50d98bc8b5ec29addf602a0d20b
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-01-26 15:11:24 -0600

    while loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,10 +1,27 @@
 console.log("Just making sure it works");
-function showMultiplicationTable(input) {
-   var i = 1;
-   while (i <= 10) {
-        console.log(input + " * " + i + " = " + (i * input));
-        i++;
-   }
-}
+// function showMultiplicationTable(input) {
+//    var i = 1;
+//    while (i <= 10) {
+//         console.log(input + " * " + i + " = " + (i * input));
+//         i++;
+//    }
+// }
+//
+// showMultiplicationTable(7);
+for (var i = 20; i < 200; i++);
+    var randomNumber = 20;
+    for (var x = 20; x < Math.floor((Math.random() * 200) + 20); x++) {
+        randomNumber++;
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + " is even!");
+        }   else if (randomNumber % 2 !== 0) {
+            console.log(randomNumber + " is odd!");
+        }
 
-showMultiplicationTable(7);
+
+    }
+
+
+    //
+
+
