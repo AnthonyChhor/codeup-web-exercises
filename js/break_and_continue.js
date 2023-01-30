9146144063c7d1e055abec79b123dc104c790523
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-01-30 13:53:24 -0600

    loops exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,22 +1,22 @@
-// let userNumber;
-//
-// while(true) {
-//     userNumber = prompt("Enter an odd number between 1 and 50:");
-//     if (userNumber % 2 !== 0 && userNumber >= 1 && userNumber <= 50) {
-//         break;
-//     }
-//     alert("Invalid input! Please enter an ODD number between 1 and 50.");
-// }
-//
-// console.log("Number to skip is: " + userNumber);
-//
-// for (let i = 1; i <= 50; i++) {
-//     if (i % 2 !== 0) {
-//         if(i === Number(userNumber)) {
-//             console.log("Yikes! Skipping number: " + i);
-//             continue;
-//         }
-//         console.log("Here is an odd number: " + i);
-//     }
-// }
+let userNumber;
+
+while(true) {
+    userNumber = prompt("Enter an odd number between 1 and 50:");
+    if (userNumber % 2 !== 0 && userNumber >= 1 && userNumber <= 50) {
+        break;
+    }
+    alert("Invalid input! Please enter an ODD number between 1 and 50.");
+}
+
+console.log("Number to skip is: " + userNumber);
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 !== 0) {
+        if(i === Number(userNumber)) {
+            console.log("Yikes! Skipping number: " + i);
+            continue;
+        }
+        console.log("Here is an odd number: " + i);
+    }
+}
 
--- js/while.js
+++ js/while.js
@@ -4,3 +4,19 @@ while (i <= 65536) {
     console.log(i);
     i *= 2;
 }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var soldCones = Math.floor(Math.random() * 5) + 1;
+var newAmountOfCones = allCones - soldCones;
+do {
+    console.log( allCones + " is the amount of cones you have!");
+    console.log(soldCones + " cones were sold!");
+    allCones -= soldCones;
+    console.log("Your new amount of cones is " + (allCones -= soldCones) + "!")
+    if (allCones <= 0) {
+        console.log("Sorry, can't sell you " + allCones + " cones.");
+    }
+
+} while(allCones > 0);
+
+
